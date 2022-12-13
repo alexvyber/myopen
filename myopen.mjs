690f452a3ece727b8aa4f0beacc5b3c5a628eb95
Alexey Sokolov <alexvyber@gmail.com>
2022-12-13 11:48:34 +0300

    init

---
+++ myopen.mjs
@@ -0,0 +1,24 @@
+import open from "open";
+
+const openArr = [
+  {
+    name: "gh",
+    href: "https://github.com/alexvyber",
+  },
+  {
+    name: "node", //
+    href: "https://nodejs.dev/",
+  },
+  {
+    name: "1000",
+    href: "https://github.com/alexvyber/1000DaysOfCode",
+  },
+];
+
+try {
+  const target = openArr.find((item) => item.name == process.argv[2]);
+  await open(target.href);
+} catch (e) {
+  console.error("Your shit wasn't found");
+  process.exit(1)
+}
---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+
+module.exports = {
+  printWidth: 80,
+  semi: false,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "none",
+  singleQuote: false,
+  bracketSameLine: true
+}
