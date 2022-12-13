2477174e7eae97e43495aca4e01238a8620e18f4
Alexey Sokolov <alexvyber@gmail.com>
2022-12-13 11:49:08 +0300

    formatted

--- myopen.mjs
+++ myopen.mjs
@@ -1,24 +1,24 @@
-import open from "open";
+import open from "open"
 
 const openArr = [
   {
     name: "gh",
-    href: "https://github.com/alexvyber",
+    href: "https://github.com/alexvyber"
   },
   {
     name: "node", //
-    href: "https://nodejs.dev/",
+    href: "https://nodejs.dev/"
   },
   {
     name: "1000",
-    href: "https://github.com/alexvyber/1000DaysOfCode",
-  },
-];
+    href: "https://github.com/alexvyber/1000DaysOfCode"
+  }
+]
 
 try {
-  const target = openArr.find((item) => item.name == process.argv[2]);
-  await open(target.href);
+  const target = openArr.find(item => item.name == process.argv[2])
+  await open(target.href)
 } catch (e) {
-  console.error("Your shit wasn't found");
+  console.error("Your shit wasn't found")
   process.exit(1)
 }
